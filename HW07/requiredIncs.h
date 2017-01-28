fa3239cfa13a507ff7217da83b3fc96a9ded79a8
M. Selim Dursun <muhammed_dru@hotmail.com>
2017-01-28 14:50:35 +0200

    Create requiredIncs.h

---
+++ HW07/requiredIncs.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <map>
+#include "BigramMap.h"
+#include "BigramMap.cpp"
+#include "BigramDyn.h"
+#include "BigramDyn.cpp"
+#include "Exception.h"
+#include <stdexcept>
+
+using namespace std;
