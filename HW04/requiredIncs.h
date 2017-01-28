7b5615d8074a72c8ef62659c416fc3aec4f94c5a
M. Selim Dursun <muhammed_dru@hotmail.com>
2017-01-28 14:47:06 +0200

    Create requiredIncs.h

---
+++ HW04/requiredIncs.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream> 
+#include <vector>
+#include <sstream>
+//header files
+#include "Computer.h"
+#include "CPUProgram.h"
+#include "Instruction.h"
+#include "CPU.h"
+#include "Memory.h"
+
+using namespace std;
