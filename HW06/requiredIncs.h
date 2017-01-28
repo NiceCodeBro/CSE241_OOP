26f189598a4d8da8f43298e9b91969d1446d61f3
M. Selim Dursun <muhammed_dru@hotmail.com>
2017-01-28 14:49:39 +0200

    Create requiredIncs.h

---
+++ HW06/requiredIncs.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream> 
+#include <vector>
+#include <sstream>
+#include <cstdlib>
+//header files
+#include "Computer.h"
+#include "CPUProgramDyn.h"
+#include "Instruction.h"
+#include "CPU.h"
+#include "Memory.h"
+using namespace std;
+using namespace CPUProgramNamespace;
